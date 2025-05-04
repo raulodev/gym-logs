d9e9b5217660d4aaf16de4af82e3d6c5c6fbc7b5
raulodev <raulodev@gmail.com>
2025-05-04 10:16:57 -0400

    center pie Chart and show default message

--- app/src/main/java/com/raulodev/gymlogs/screens/ChartScreen.kt
+++ app/src/main/java/com/raulodev/gymlogs/screens/ChartScreen.kt
@@ -3,6 +3,7 @@ package com.raulodev.gymlogs.screens
 import android.icu.util.Calendar
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -18,8 +19,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,6 +53,7 @@ fun ChartScreen(
     db: AppDatabase? = null, navegationController: NavHostController? = null
 ) {
 
+    var loading by remember { mutableStateOf(true) }
     val pieChartData = remember { mutableStateListOf<PieChartData>() }
     val barChartData = remember { mutableStateListOf<BarData>() }
 
@@ -132,6 +138,7 @@ fun ChartScreen(
             withContext(Dispatchers.IO) {
                 countMembers()
                 getPayments()
+                loading = false
             }
         } catch (e: Exception) {
             Log.e("DevLogs", "Error: ${e.message}")
@@ -185,7 +192,7 @@ fun ChartScreen(
             }
             if (pieChartData.isNotEmpty()) {
                 item {
-                    Column {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
                         Text(
                             "Total de usuarios",
                             textAlign = TextAlign.Center,
@@ -200,6 +207,16 @@ fun ChartScreen(
                                 .size(300.dp)
                                 .padding(4.dp)
                         )
+                    }
+                }
+            }
+            if (pieChartData.isEmpty() && barChartData.isEmpty() && !loading) {
+                item {
+                    Box(
+                        modifier = Modifier.fillParentMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Sin datos")
                     }
                 }
             }
